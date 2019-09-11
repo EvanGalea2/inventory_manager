069fafc5f1b13970fc43f2e5da1d46e4dc793cb2
EvanGalea2 <evanjamesgalea@gmail.com>
2019-09-11 09:13:54 -0400

    Updated button functionality

--- MainMenuButtons.py
+++ MainMenuButtons.py
@@ -12,14 +12,18 @@ class MenuButtons:
         print("add")
         gui.title("Add New Item(s)")
         gui.geometry("360x120")
-        frame = Frame(gui, background="red")
+        frame = Frame(gui, background="red").grid(row = 0, column = 0)
         gridRow = 0
         fields = inventory.getFields("items", None)
         for field in fields:
             Label(frame, text = field, width = 10).grid(row = gridRow, column = 0)
-            Entry(frame).grid(gridRow, column = 1)
+            Entry(frame).grid(row = gridRow, column = 1)
             gridRow += 1
+        print("test")
+        enterButton = Button(frame, width=10).grid(row = 0, column = 0)
+
         gui.mainloop()
+
 
     def close(self):
         gui.close()
